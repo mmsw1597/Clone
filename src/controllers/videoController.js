0ecaa028f018cc02583df383e3711d7db41c09b9
mmsw1597 <mmsw1597@naver.com>
2023-02-09 20:55:15 +0900

    refactor: 컨트롤러와 라우터를 분리

---
+++ src/controllers/videoController.js
@@ -0,0 +1,5 @@
+export const trending = (req, res) => res.send("Home Page");
+export const watch = (req, res) => res.send("Watch");
+export const edit = (req, res) => res.send("Edit");
+
+//export default는 하나밖에 export하지 못함
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,12 +1,16 @@
 import express from "express";
+import { join } from "../controllers/userController";
+import { trending } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
+globalRouter.get("/", trending);
+globalRouter.get("/join", join);
 
 //export
 //default export 를 사용하여 다른 곳에서 import할때 다른 변수 이름을 사용가능
 //하지만 보통은 혼란은 방지하기 위해 같은 이름을 사용
+//default export는 한개밖에 export하지 못함
+//non-default export를 사용하여 여러개를 export할 수 있음
+//이때 import 하는 곳에선 오브젝트로 해당 export 모듈을 받을 수 있음
 export default globalRouter;
