efc542d0e9f20757d5a7ab93262fd1fa771e8dc6
mmsw1597 <mmsw1597@naver.com>
2023-02-09 22:36:41 +0900

    fix: URL 파라미터, 정규표현식

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,9 +3,14 @@ import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
+//URL 파라미터
+//URL안에 변수를 포함시킬 수 있게 함
+//순서가 중요함 변수 파라미터가 upload url 위로 오면 아래에 있는 upload URL로 갈때 upload 문자열 자체를 id로 받아버림
+//정규표현식을 통해 id가 무조건 숫자임을 명시하면 순서를 바꿔도 상관없음
+//옆에 id를 붙인이유는 파라미터를 변수로 불러와야 하기 때문. 그래서 id라는 이름을 붙여줌
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
