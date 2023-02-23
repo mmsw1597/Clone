4be6846c778d813c3ff1e45967986f102b21e6d5
mmsw1597 <mmsw1597@naver.com>
2023-02-24 03:41:22 +0900

    Recap: 복습 끝! 자세한건 주석 참고

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -49,6 +49,19 @@ export const getUpload = (req, res) => {
 };
 export const postUpload = (req, res) => {
   //비디오를 비디오 배열에 추가할 예정
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "now",
+    views: 0,
+    id: videos.length,
+  };
+  videos.push(newVideo);
+  //1. post request를 받을 컨트롤러임.
+  //2. redirect를 하여 브라우저에게 홈페이지로 가도록 할 예정. 이때 브라우저는 총 2번의 url을 통과하게 될거임. 하나는 upload 하나는 홈페이지
+  //3. request의 body를 통해 input값을 받아올 수 있음. 이때 input은 반드시 name 속성 값을 명시해야 됨.
   return res.redirect("/");
 };
 //export default는 하나밖에 export하지 못함
