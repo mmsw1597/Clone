8fa9d9dcaedff5cbd57b8020bd75e2701d1e60e3
mmsw1597 <mmsw1597@naver.com>
2023-02-26 16:38:04 +0900

    Feature: mongoose 미들웨어, static이란 미들웨어는 특정 행동 (save, update)를 하기 전이나 후에 추가로 어떤 로직을 처리해주는 함수 미들웨어는 예를들어 새로운 유저를 생성한다고 할때 생성 후 비밀번호를 암호화해야 한다면 이때 save에 반응하는 암호화 미들웨어를 추가하면 된다 static은 특정 모델에 종속되는 함수이다. static이 없다면 모델과 함수를 둘 다 import 하여 코드를 작성해야 하지만 static이 있는 경우 모델만 import하면 그에 종속된 함수도 같이 가져오게 되는 것 내가 작성한 코드에선 해쉬태그의 형식을 바꾸는 것인데 save 미들웨어를 사용할 경우 오로지 create에만 반응하고 update에는 반응하지 않았다 따라서 static을 사용하여 처리한 것

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -39,9 +39,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -55,9 +53,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags
-        .split(",")
-        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
