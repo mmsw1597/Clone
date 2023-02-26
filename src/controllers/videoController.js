5ef3399290fc02232aa434541daf6db6d2fe7f8e
mmsw1597 <mmsw1597@naver.com>
2023-02-26 14:21:30 +0900

    Feautre: Add Validation, Exception

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -29,14 +29,28 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   //비디오를 비디오 배열에 추가할 예정
-  const { title } = req.body;
-  videos.push(newVideo);
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      meta: {
+        views: 0,
+        rating: 0,
+      },
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
   //1. post request를 받을 컨트롤러임.
   //2. redirect를 하여 브라우저에게 홈페이지로 가도록 할 예정. 이때 브라우저는 총 2번의 url을 통과하게 될거임. 하나는 upload 하나는 홈페이지
   //3. request의 body를 통해 input값을 받아올 수 있음. 이때 input은 반드시 name 속성 값을 명시해야 됨.
-  return res.redirect("/");
 };
 //export default는 하나밖에 export하지 못함
 //redirect는 특정 url로 이동하게끔 하도록 함
