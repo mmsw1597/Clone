32c7ae03afeaa9d10d6e7b948014ec0cf6605bb9
mmsw1597 <mmsw1597@naver.com>
2023-02-26 16:03:13 +0900

    Feature: 데이터 수정 컨트롤러를 DB와 연동 exists와 findByIdAndUpdate의 차이점은 exists는 인자로 filter를 받음 예를 들어 특정 제목을 찾거나 특정 아이디를 찾거나 그리고 exists는 true나 false를 반환함 findByIdAndUpdate는 검색 조건을 오로지 아이디만 받고 데이터 객체를 반환함 컨트롤러의 기능에 따라서 두 함수를 구분해서 사용하는 것이 좋다

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -29,9 +29,20 @@ export const getEdit = async (req, res) => {
   }
   return res.render("edit", { pageTitle: `Edit ${video.title}`, video });
 };
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.exists({ _id: id });
+  if (!video) {
+    return res.render("404", { pageTitle: "Video Not Found" });
+  }
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: hashtags
+      .split(",")
+      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+  });
   return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
@@ -44,6 +55,9 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
+      hashtags: hashtags
+        .split(",")
+        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
     });
     return res.redirect("/");
   } catch (error) {
