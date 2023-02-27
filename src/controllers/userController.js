e117e7678843bbabc9901079ccbd47e1c192b51c
mmsw1597 <mmsw1597@naver.com>
2023-02-27 23:37:37 +0900

    Feature: form 중복 검증 일부 추가

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,8 +3,22 @@ import User from "../models/User";
 export const getJoin = (req, res) =>
   res.render("join", { pageTitle: "Create Account" });
 export const postJoin = async (req, res) => {
-  console.log(req.body);
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle: "Create Account",
+      errorMessage: "비밀번호가 서로 일치하지 않습니다",
+    });
+  }
+
+  //지금은 연습이니까 이렇게 만듦 (username, email 둘 중 하나가 중복이면 에러메시지 전송. 실제로 이렇게 구현하면 뭐가 중복인지 유저는 모름)
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle: "Create Account",
+      errorMessage: "해당 닉네임은 이미 사용 중입니다.",
+    });
+  }
   await User.create({
     name,
     username,
